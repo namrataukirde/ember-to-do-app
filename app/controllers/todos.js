17c8fe14bae05097a7002b83ae6dba06f48c7a60
namrataukirde <namrata.ukirde@kiprosh.com>
2016-08-17 15:56:09 +0530

    adding input box for edit on double click on label

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -19,8 +19,23 @@ export default Ember.Controller.extend({
     markComplete(todo) {
       // todo.set('isCompleted', true);
       todo.toggleProperty('isCompleted');
-    }
+    },
+
+    editToDo(todo){
+      todo.set('isEditing', true)
+    },
+
+    isEditing: false,
   },
+
+  remaining: Ember.computed("model.@each.isCompleted", function(){
+    return this.get('model').filterBy('isCompleted', false).get('length');
+  }),
+
+  inflection: Ember.computed('remaining', function(){
+    var remaining = this.get('remaining');
+    return remaining == 1 ? 'item' : 'items'
+  }),
 
   // isCompleted: Ember.computed("model.isCompleted", function(key, value){
   //   var model = this.get('model');
